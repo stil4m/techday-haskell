15976377deec67ffdcf835e135bd2f26944656c2
Mats Stijlaart <m.stijlaart@avisi.nl>
2015-10-30 09:06:14 +0100

    Added example fib

--- hands-on/matstijl/Presentation.hs
+++ hands-on/matstijl/Presentation.hs
@@ -6,3 +6,9 @@ where
 
 even' :: Integer -> Bool
 even' x = x `mod` 2 == 0
+
+fib :: Integer -> Integer
+fib 0 = 1
+fib 1 = 1
+fib n = fib' (0,1,1) (n-1) where
+  fib' (x,y,z) n = if n == 0 then z else fib' (y,z,y+z) (n-1)
